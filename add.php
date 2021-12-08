8a6d10c657417c10ca52709e1daf03a8de5a616e
Arshed Ahmed <arshedahmedmail@gmail.com>
2021-12-08 22:53:40 +0530

    error handling

--- add.php
+++ add.php
@@ -1,31 +1,37 @@
 <?php 
 
+ 	$errors = array('email'=>'','title'=>'','ingredients'=>'');
+
 	if(isset($_POST['submit'])){
 		if (empty($_POST['email'])) {
-			echo 'email is required <br />';
+			$errors['email'] = 'Email is required';
 		}else{
 			$email = $_POST['email'];
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				echo 'Email must be a valid email address <br />';
+				$errors['email'] = 'Email must be a valid email address';
 			}
 		}
 
 		if (empty($_POST['title'])) {
-			echo 'title is required <br />';
+			$errors['title'] = 'Title is required';
 		}else{
 			$title = $_POST['title'];
 			if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-				echo 'Title must be letters and spaces only';
+				$errors['title'] = 'Title must be letters and spaces only';
 			}
 		}
 
 		if (empty($_POST['ingredients'])) {
-			echo 'At least one ingredient is required <br />';
+			$errors['ingredients'] ='At least one ingredient is required';
 		}else{
 			$ingredients = $_POST['ingredients'];
 			if (!preg_match('/(^[a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-				echo 'Ingredients must be a comma seperated list';
+				$errors['ingredients'] ='Ingredients must be a comma seperated list';
 			}
+		}
+
+		if (!array_filter($errors)) {
+			header('Location: index.php');
 		}
 	}
 
@@ -41,10 +47,13 @@
 		<form class="white" action="add.php" method="POST">
 			<label>Your Email</label>
 			<input type="text" name="email">
+			<div class="red-text"><?php echo $errors['email'] ?></div>
 			<label>Pizza Title</label>
 			<input type="text" name="title">
+			<div class="red-text"><?php echo $errors['title'] ?></div>
 			<label>Ingredients (comma separated)</label>
 			<input type="text" name="ingredients">
+			<div class="red-text"><?php echo $errors['ingredients'] ?></div>
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
