c34b81be0f049bc2afe165422fa0a9c0e5d491cc
Arshed Ahmed <arshedahmedmail@gmail.com>
2021-12-08 20:35:21 +0530

    prevent xss attack

--- add.php
+++ add.php
@@ -1,15 +1,9 @@
 <?php 
 
-	// if(isset($_GET['submit'])){
-	// 	echo $_GET['email'] . '<br />';
-	// 	echo $_GET['title'] . '<br />';
-	// 	echo $_GET['ingredients'] . '<br />';
-	// }
-
 	if(isset($_POST['submit'])){
-		echo $_POST['email'] . '<br />';
-		echo $_POST['title'] . '<br />';
-		echo $_POST['ingredients'] . '<br />';
+		echo htmlspecialchars($_POST['email']) . '<br />';
+		echo htmlspecialchars($_POST['title']) . '<br />';
+		echo htmlspecialchars($_POST['ingredients']) . '<br />';
 	}
 
 ?>
