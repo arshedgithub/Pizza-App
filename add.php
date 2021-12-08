74002a6965930ee0b5c6f5863d021d23ef737dbc
Arshed Ahmed <arshedahmedmail@gmail.com>
2021-12-08 22:24:46 +0530

    validation with regEx

--- add.php
+++ add.php
@@ -4,19 +4,28 @@
 		if (empty($_POST['email'])) {
 			echo 'email is required <br />';
 		}else{
-			echo htmlspecialchars($_POST['email']) . '<br />';
+			$email = $_POST['email'];
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				echo 'Email must be a valid email address <br />';
+			}
 		}
 
 		if (empty($_POST['title'])) {
 			echo 'title is required <br />';
 		}else{
-			echo htmlspecialchars($_POST['title']) . '<br />';
+			$title = $_POST['title'];
+			if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+				echo 'Title must be letters and spaces only';
+			}
 		}
 
 		if (empty($_POST['ingredients'])) {
-			echo 'at least one ingredient is required <br />';
+			echo 'At least one ingredient is required <br />';
 		}else{
-			echo htmlspecialchars($_POST['ingredients']) . '<br />';
+			$ingredients = $_POST['ingredients'];
+			if (!preg_match('/(^[a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+				echo 'Ingredients must be a comma seperated list';
+			}
 		}
 	}
 
